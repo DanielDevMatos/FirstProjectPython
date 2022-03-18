fd1a15e0cece38e14f8a00a9ea0f1198004d68a7
Daniel <danielhunterteo@gmail.com>
2022-03-18 14:21:06 -0300

    integração com codecov

---
+++ projectpython/spam/envidor_de_email.py
@@ -0,0 +1,8 @@
+class Enviador:
+    def enviar(self, remetente, destinatario, assunto, corpo):
+        if '@' not in remetente:
+            raise EmailInvalido(f'Email de remetente Invalido: {remetente}')
+        return remetente
+
+class EmailInvalido(Exception):
+    pass
---
+++ projectpython/tests/test_spam/test_envidor_de_email.py
@@ -0,0 +1,38 @@
+import pytest as pytest
+
+from projectpython.spam.envidor_de_email import Enviador, EmailInvalido
+
+
+def test_criar_enviador_email():
+    enviador = Enviador()
+    assert enviador is not None
+
+@pytest.mark.parametrize(
+    'remetente',
+    ['renzo@python.pro.br', 'foo@bar.com.br']
+)
+
+def test_remetente(remetente):
+    enviador = Enviador()
+    resultado = enviador.enviar(
+        remetente,
+        'luciano@python.pro.br',
+        'Cursos Python Pro',
+        'Primeira turma Guido Von Rossum aberta.'
+    )
+    assert remetente in resultado
+
+@pytest.mark.parametrize(
+    'remetente',
+    ['', 'foo']
+)
+
+def test_remetente(remetente):
+    enviador = Enviador()
+    with pytest.raises(EmailInvalido):
+            enviador.enviar(
+            remetente,
+            'luciano@python.pro.br',
+            'Cursos Python Pro',
+            'Primeira turma Guido Von Rossum aberta.'
+            )
